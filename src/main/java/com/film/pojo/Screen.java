83d0db000191dcda33e17c51035214322635bf61
JF <2636976144@qq.com>
2020-07-13 17:40:48 +0800

    wjf

---
+++ src/main/java/com/film/pojo/Screen.java
@@ -0,0 +1,14 @@
+package com.film.pojo;
+
+import lombok.Data;
+
+@Data
+public class Screen {
+
+    private int sid;
+    private int cid;
+    private int mid;
+    private int viid;
+    private String stime;
+
+}
