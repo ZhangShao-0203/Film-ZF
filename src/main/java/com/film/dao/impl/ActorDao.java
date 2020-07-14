7933cde6036ebc6bceaaec94e3e3af6ee1c4ef31
ZhangShao <2636976144@qq.com>
2020-07-14 10:04:46 +0800

    ZST

--- src/main/java/com/film/dao/impl/ActorDao.java
+++ src/main/java/com/film/dao/impl/ActorDao.java
@@ -1,6 +1,33 @@
 package com.film.dao.impl;
 
 import com.film.dao.IActorDao;
+import com.film.pojo.Actor;
+
+import java.util.List;
 
 public class ActorDao implements IActorDao {
+    @Override
+    public int add(Actor actor) {
+        return 0;
+    }
+
+    @Override
+    public Actor get(int id) {
+        return null;
+    }
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+
+    @Override
+    public int update(Actor actor) {
+        return 0;
+    }
+
+    @Override
+    public List<Actor> list() {
+        return null;
+    }
 }
--- src/main/java/com/film/dao/impl/AdminDao.java
+++ src/main/java/com/film/dao/impl/AdminDao.java
@@ -1,6 +1,33 @@
 package com.film.dao.impl;
 
 import com.film.dao.IAdminDao;
+import com.film.pojo.Admin;
+
+import java.util.List;
 
 public class AdminDao implements IAdminDao {
+    @Override
+    public int add(Admin admin) {
+        return 0;
+    }
+
+    @Override
+    public Admin get(int id) {
+        return null;
+    }
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+
+    @Override
+    public int update(Admin admin) {
+        return 0;
+    }
+
+    @Override
+    public List<Admin> list() {
+        return null;
+    }
 }
